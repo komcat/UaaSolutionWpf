bbaa40437dd1ce5f2af72bea8dc198d254a888a6
Komgrit Sungkhaphong <komgritclick@gmail.com>
2025-01-30 20:25:22 -0800

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UaaSolutionWpf.Controls;
+
+namespace UaaSolutionWpf
+{
+
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private StackPanel leftPanel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeControls();
+        }
+
+
+        private void InitializeControls()
+        {
+            // Find the StackPanel from the Grid
+            leftPanel = (StackPanel)this.FindName("LeftStackPanel");
+
+            // If the StackPanel wasn't found by name, we can get it from the Grid
+            if (leftPanel == null)
+            {
+                // Get the main grid
+                Grid mainGrid = (Grid)this.Content;
+                // Get the first StackPanel in column 0
+                leftPanel = (StackPanel)mainGrid.Children[0];
+            }
+
+            // Create and add the TEC Controller
+            TECController tecController = new TECController();
+            leftPanel.Children.Add(tecController);
+        }
+
+    }
+}

bdf7d9873c82d98552f921689e0d7f81ea45cd4e
Komgrit Sungkhaphong <komgritclick@gmail.com>
2025-02-02 16:23:27 -0800

    status to color converters

---
+++ Converters/StatusToColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace UaaSolutionWpf.Converters
+{
+    public class StatusToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool status)
+            {
+                return status ? new SolidColorBrush(Colors.LightGreen) : new SolidColorBrush(Colors.Red);
+            }
+            return new SolidColorBrush(Colors.Gray);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

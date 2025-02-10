9e26ad58ecb82bcd17efa2943169ca011d2ca910
Komgrit Sungkhaphong <komgritclick@outlook.com>
2025-02-10 14:07:04 -0800

    upgrade visual for VacBaseControl

--- Controls/VacBaseControl.xaml.cs
+++ Controls/VacBaseControl.xaml.cs
@@ -1,16 +1,46 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace UaaSolutionWpf.Controls
 {
     public partial class VacBaseControl : UserControl
     {
+        private readonly LinearGradientBrush _activeStatusBrush;
+        private readonly LinearGradientBrush _inactiveStatusBrush;
+
         public VacBaseControl()
         {
             InitializeComponent();
 
+            // Initialize status indicator brushes
+            _activeStatusBrush = new LinearGradientBrush
+            {
+                StartPoint = new Point(0, 0),
+                EndPoint = new Point(1, 0)
+            };
+            _activeStatusBrush.GradientStops.Add(new GradientStop(Color.FromRgb(67, 97, 238), 0));
+            _activeStatusBrush.GradientStops.Add(new GradientStop(Color.FromRgb(58, 12, 163), 1));
+
+            _inactiveStatusBrush = new LinearGradientBrush
+            {
+                StartPoint = new Point(0, 0),
+                EndPoint = new Point(1, 0)
+            };
+            _inactiveStatusBrush.GradientStops.Add(new GradientStop(Color.FromRgb(233, 236, 239), 0));
+            _inactiveStatusBrush.GradientStops.Add(new GradientStop(Color.FromRgb(233, 236, 239), 1));
+
             // Add click event handlers
             VacBaseOnButton.Click += (s, e) => VacBaseState = true;
             VacBaseOffButton.Click += (s, e) => VacBaseState = false;
+
+            // Add animation handlers
+            VacBaseOnButton.PreviewMouseDown += Button_PreviewMouseDown;
+            VacBaseOnButton.PreviewMouseUp += Button_PreviewMouseUp;
+            VacBaseOffButton.PreviewMouseDown += Button_PreviewMouseDown;
+            VacBaseOffButton.PreviewMouseUp += Button_PreviewMouseUp;
         }
 
         private bool _vacBaseState;
@@ -20,19 +50,48 @@ namespace UaaSolutionWpf.Controls
             set
             {
                 _vacBaseState = value;
-                UpdateButtonStates();
+                UpdateControlStates();
             }
         }
 
-        private void UpdateButtonStates()
+        private void UpdateControlStates()
         {
-            VacBaseOnButton.Background = VacBaseState ?
-                System.Windows.Media.Brushes.DodgerBlue :
-                System.Windows.Media.Brushes.LightGray;
+            // Update status indicator
+            StatusIndicator.Background = VacBaseState ? _activeStatusBrush : _inactiveStatusBrush;
 
-            VacBaseOffButton.Background = !VacBaseState ?
-                System.Windows.Media.Brushes.LightGray :
-                System.Windows.Media.Brushes.Transparent;
+            // Update button states
+            VacBaseOnButton.IsEnabled = !VacBaseState;
+            VacBaseOffButton.IsEnabled = VacBaseState;
+
+            // Animate the change
+            var fadeAnimation = new DoubleAnimation
+            {
+                To = VacBaseState ? 1 : 0.6,
+                Duration = TimeSpan.FromMilliseconds(200)
+            };
+
+            if (VacBaseState)
+            {
+                VacBaseOnButton.BeginAnimation(OpacityProperty, fadeAnimation);
+                VacBaseOffButton.Opacity = 1;
+            }
+            else
+            {
+                VacBaseOffButton.BeginAnimation(OpacityProperty, fadeAnimation);
+                VacBaseOnButton.Opacity = 1;
+            }
+        }
+
+        private void Button_PreviewMouseDown(object sender, RoutedEventArgs e)
+        {
+            var animation = (Storyboard)FindResource("PressAnimation");
+            animation.Begin((Button)sender);
+        }
+
+        private void Button_PreviewMouseUp(object sender, RoutedEventArgs e)
+        {
+            var animation = (Storyboard)FindResource("ReleaseAnimation");
+            animation.Begin((Button)sender);
         }
     }
 }
